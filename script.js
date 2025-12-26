252a1b80ecbaae9e189d8d8308c4e20b0e8f129c
Marie Rajan <6733490+majacob@users.noreply.github.com>
2025-12-26 15:23:49 -0500

    updates

---
+++ script.js
@@ -0,0 +1,40 @@
+mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v11',
+  center: [-78.6382, 35.7796], // Wake County-ish
+  zoom: 10
+});
+
+map.on('load', () => {
+  map.addSource('schools', {
+    type: 'geojson',
+    data: 'data/schools.geojson'
+  });
+
+  // Magnet schools
+  map.addLayer({
+    id: 'magnet-schools',
+    type: 'circle',
+    source: 'schools',
+    filter: ['==', ['get', 'magnet'], true],
+    paint: {
+      'circle-radius': 6,
+      'circle-color': '#d7191c'
+    }
+  });
+
+  // Charter schools
+  map.addLayer({
+    id: 'charter-schools',
+    type: 'circle',
+    source: 'schools',
+    filter: ['==', ['get', 'charter'], true],
+    paint: {
+      'circle-radius': 6,
+      'circle-color': '#2c7bb6'
+    }
+  });
+});
+
